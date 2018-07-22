d8572388188148ab777bd2d1133bee50c21ae5f6
walli <wallypark710@gmail.com>
2018-07-23 01:08:35 +0900

    add random function

--- testJavaScript.js
+++ testJavaScript.js
@@ -1,2 +1,13 @@
 console.log("hello, JavaScript and github");
 console.log("another commit test");
+
+var randomString = function( length ) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i = 0; i < length; i++ ) {
+	text += possible.charAt( Math.floor( Math.random() * possible.length ) );
+    }
+
+    return text;
+}
